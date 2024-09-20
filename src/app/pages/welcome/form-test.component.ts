fedac84fb7de8858e458069dda4f8eb252af5f59
devkbm <devkbm0417@gmail.com>
2024-09-20 23:58:05 +0900

    2024-09-20

--- src/app/pages/welcome/form-test.component.ts
+++ src/app/pages/welcome/form-test.component.ts
@@ -4,37 +4,43 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzFormItemComponent } from "./nz-form-item.component";
 
 @Component({
   selector: 'app-form-test',
   standalone: true,
   imports: [
-    CommonModule, FormsModule,  ReactiveFormsModule,
-    NzGridModule, NzFormModule, NzInputModule
-  ],
+    CommonModule, FormsModule, ReactiveFormsModule,
+    NzGridModule, NzFormModule, NzInputModule,
+    NzFormItemComponent
+],
   template: `
+    {{fg.getRawValue() | json}}
+    <ng-template #errorTpl let-control>
+      @if (control.hasError('required')) {
+        필수 입력 값입니다.
+      }
+    </ng-template>
+
     <form nz-form [formGroup]="fg" nzLayout="vertical">
-    <div nz-row nzGutter="8">
-      <div nz-col nzSpan="8">
-        <nz-form-item>
-          <nz-form-label nzFor="email">E-mail</nz-form-label>
-          <nz-form-control>
-            <input nz-input name="email" type="email" id="email" />
-          </nz-form-control>
-        </nz-form-item>
+      <div nz-row nzGutter="8">
+        <div nz-col nzSpan="8">
+          <nz-form-item>
+            <nz-form-label nzFor="input_text">E-mail</nz-form-label>
+            <nz-form-control [nzErrorTip]="errorTpl" >
+              <input nz-input id="input_text" name="input_text" formControlName="input_text"/>
+            </nz-form-control>
+          </nz-form-item>
+        </div>
+
+        <div nz-col nzSpan="8">
+          <app-nz-form-item [itemId]="'input_text2'" [label]="'test2'">
+            <nz-form-control nzHasFeedback [nzErrorTip]="errorTpl">
+              <input nz-input id="input_text2" name="input_text2" formControlName="input_text2" />
+            </nz-form-control>
+          </app-nz-form-item>
+        </div>
       </div>
-
-      <div nz-col nzSpan="8">
-        <nz-form-item>
-          <nz-form-label nzFor="email2">E-mail2</nz-form-label>
-          <nz-form-control>
-            <input nz-input name="email2" type="email2" id="email2" />
-          </nz-form-control>
-        </nz-form-item>
-      </div>
-    </div>
-
-
     </form>
   `,
   styles: `
@@ -42,6 +48,7 @@ import { NzGridModule } from 'ng-zorro-antd/grid';
 })
 export class FormTestComponent {
   fg: FormGroup = inject(FormBuilder).group({
-    input_text: ['test', [ Validators.required ]]
+    input_text: ['test', [ Validators.required ]],
+    input_text2: ['test', [ Validators.required ]],
   });
 }
---
+++ src/app/pages/welcome/nz-form-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, input, TemplateRef } from '@angular/core';
+
+import { AbstractControl, FormControl, FormControlName, FormsModule, NgModel } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+
+@Component({
+  selector: 'app-nz-form-item',
+  standalone: true,
+  imports: [FormsModule, NzFormModule],
+  template: `
+    <nz-form-item>
+      <nz-form-label [nzFor]="itemId()" [nzRequired]="required()">
+        {{label()}}
+      </nz-form-label>
+      <ng-content></ng-content>
+    </nz-form-item>
+  `,
+  styles: `
+  `
+})
+export class NzFormItemComponent {
+
+  itemId = input<string>('');
+  label = input<string>('');
+  required = input<boolean>(false);
+
+  nzErrorTip = input<string | TemplateRef<{$implicit: AbstractControl | NgModel;}>>();
+
+  nzValidateStatus = input<string | AbstractControl<any, any> | FormControlName | NgModel>('');
+}
